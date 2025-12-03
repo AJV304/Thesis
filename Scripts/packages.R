7271b04aac8952d2c1704971c9d1864f747e2fed
AJV304 <100213034+AJV304@users.noreply.github.com>
2025-12-03 18:02:03 +0100

    Cleaning up

--- Scripts/packages.R
+++ Scripts/packages.R
@@ -1,4 +1,25 @@
 #Packages
 
-install.packages("DiagrammeR")
+#install.packages("dplyr")
+#install.packages("DiagrammeR")
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
 library(DiagrammeR)
+library(mvtnorm)
+library(stats)
+
+#------------
+#Diagram
+
+```{r}
+#| echo: false
+
+#flow-chart for situations
+graph <- DiagrammeR(" graph TD; A(Scenario) --> F(Baseline Condition);A --> G(Deviation condition);G --> J(Deviation value); G --> K(Pick best value);")
+
+# Save the diagram as a PNG file
+export_graph(graph, file_name = "process-phases.png", file_type = "png")
+
+```
