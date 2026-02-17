fbda646576147484f95c7d6124d1a6007c6d3173
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-02-17 13:43:12 +0100

    renaming and restructuring code

--- Scripts/03_results/overarchingfunction.R
+++ Scripts/03_results/overarchingfunction.R
@@ -2,16 +2,17 @@
 
 thesis <- function(iter, n, b0, b1_no, b1_yes, b_z, b_d, dep) {
   
-  stats <- data.frame(matrix(ncol = 4, nrow = (9*iter)))
-  colnames(stats) <- cn
+  stats <- data.frame(matrix(ncol = 5, nrow = (9*iter)))
+  colnames(stats) <- c(cn, "condition")
   
   for (i in 1:iter) {
     
-    set.seed(i)
+    #set.seed(i)
     df <- dgm(n, b0, b1_no, b1_yes, b_z, b_d)
     result <- analysis(df, dep)
     stats[(((i-1)*9+1)):(i*9),] <- result
-    rownames(stats)[(((i-1)*9+1)):(i*9)] <- paste0(rownames(result), " iteration ", i)
+    stats$iteration[(((i-1)*9+1)):(i*9)] <- paste("iteration", i, sep = " ")
+    #rownames(stats)[(((i-1)*9+1)):(i*9)] <- paste0(rownames(result), " iteration ", i)
     
   }
   
@@ -19,9 +20,3 @@ thesis <- function(iter, n, b0, b1_no, b1_yes, b_z, b_d, dep) {
   }
 
 
-test <- thesis(iter = 5, n = 230, b0 = 0, b1_no = 0, b1_yes = 0.3, b_z = 0.06, b_d = 0.06, dep = "y_no")
-test
-      
-
-#benchmark to see how long one iteration takes
-microbenchmark(thesis(iter = 1, n = 230, b0 = 0, b1_no = 0, b1_yes = 0.3, b_z = 0.06, b_d = 0.06, dep = "y_no") , times = 100, unit = "s")
