db8bfbbfd91c0db419eb1e8201df83aab53cb317
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-03-04 14:07:58 +0100

    have the iteration column be just a number instead of 'iteration x'

--- Scripts/03_results/overarchingfunction.R
+++ Scripts/03_results/overarchingfunction.R
@@ -2,17 +2,23 @@
 
 thesis <- function(iter, n, b0, b1_no, b1_yes, b_z, b_d, dep) {
   
+  #create empty dataframe for the statistics 
   stats <- data.frame(matrix(ncol = 5, nrow = (9*iter)))
   colnames(stats) <- c(cn, "condition")
   
   for (i in 1:iter) {
     
     #set.seed(i)
+    
+    #simulate a dataset
     df <- dgm(n, b0, b1_no, b1_yes, b_z, b_d)
+    
+    #run all scenarios on the dataset
     result <- analysis(df, dep)
+    
+    #add scenarios to the stats dataset and add iteration number
     stats[(((i-1)*9+1)):(i*9),] <- result
-    stats$iteration[(((i-1)*9+1)):(i*9)] <- paste("iteration", i, sep = " ")
-    #rownames(stats)[(((i-1)*9+1)):(i*9)] <- paste0(rownames(result), " iteration ", i)
+    stats$iteration[(((i-1)*9+1)):(i*9)] <- i #paste("iteration", i, sep = " ")
     
   }
   
