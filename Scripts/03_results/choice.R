d50192b24efbce93e9b9e71230ddfb8042f6b2eb
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-03-06 16:23:54 +0100

    Add extra variable to choice output where it reports the number of significant deviations (NA if baseline is significant)

--- Scripts/03_results/choice.R
+++ Scripts/03_results/choice.R
@@ -5,7 +5,8 @@ choice <- function(scenarios){
   x <- length(unique(scenarios[,6]))
   
   #create empty data frame with 1 row per iteration
-  final <- data.frame(matrix(ncol = ncol(scenarios), nrow = x)) 
+  final <- data.frame(matrix(ncol = sum(ncol(scenarios), 1), nrow = x)) 
+  colnames(final) <- c(colnames(scenarios), "deviations")
 
   #for each iteration follow the rank order to check which result to select
   for (i in 1:x){
@@ -15,19 +16,20 @@ choice <- function(scenarios){
     #check if the baseline scenario is significant
     if (it[it$condition == "Baseline", "p.value"] < 0.05){
       #if it is, then select the baseline scenario
-      final[i,] <-it[it$condition == "Baseline",]
+      final[i,] <- it[it$condition == "Baseline",]
+      final[i, "deviations"] <- "NA"
     } else {
       if (any(it[,"p.value"] < 0.05)){
         sig <- it %>% filter(p.value < 0.05)
         final[i,] <- sig %>% slice_sample(n = 1)
+        final[i, "deviations"] <- nrow(sig)
         } else { 
           final[i,] <- it[it$condition == "Baseline",]
+          final[i,"deviations"] <- 0
           }
     }
   }
-
   
-  colnames(final) <- colnames(scenarios)
   return(final)
   }
      
