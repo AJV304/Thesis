001ab93b724e6f52bbbb0afb8878cf5a39acd3f6
AJV304 <100213034+AJV304@users.noreply.github.com>
2026-04-28 21:22:20 +0200

    i hate sankey graphs

--- Scripts/03_visuals/sankey.R
+++ Scripts/03_visuals/sankey.R
@@ -1,12 +1,11 @@
-library(plotly)
 
 # nodes — every unique label in order
 nodes <- list(
   label = c(
-    "All iterations",           # 0
-    "Baseline not significant", # 1
+    "",           # 0
+    "", # 1
     "Baseline significant",     # 2
-    "A significant deviation",  # 3
+    "",  # 3
     "No significant deviations",# 4
     "1 sig.",                   # 5
     "2 sig.",                   # 6
@@ -29,20 +28,19 @@ nodes <- list(
     "#A5D6A7",  # 5 sig.
     "#A5D6A7",  # 6 sig.
     "#A5D6A7"   # 7 sig.
-  ))
-,
+  ),
   x = c(
-    0.01,                                     # All iterations
-    0.33, 0.33,                               # level 2
-    0.66, 0.66,                               # level 3
+    0.1,                                     # All iterations
+    0.33, 0.99,                               # level 2
+    0.66, 0.99,                               # level 3
     0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99 # level 4
   ),
   # y position: spread nodes within each column
   y = c(
-    0.5,                                      # All iterations (centred)
-    0.3, 0.85,                                # Baseline not sig, Baseline sig
-    0.15, 0.55,                               # A sig deviation, No sig deviations
-    0.01, 0.1, 0.18, 0.26, 0.34, 0.40, 0.45  # 1-7 sig. (spread out)
+    0.5,          # All iterations
+    0.80, 0.30,   # Baseline not sig, Baseline sig
+    0.85, 0.65,   # A sig deviation, No sig deviations
+    0.76, 0.80, 0.84, 0.88, 0.92, 0.96, 1  # 1 sig → 7 sig
   )
 )
 
@@ -72,25 +70,39 @@ plot_ly(
   node = nodes,
   link = links
 ) |>
-  layout(
-    title = "Condition significance split",
-    font = list(size = 14)
-  )
-
-
-#test
-nodes$label <- rep("", length(nodes$label))
-
-plot_ly(type = "sankey",
-        orientation = "h",
-        node = nodes,
-        link = links) |>
-  layout(
+  plotly::layout(
+    title = "Distribution of significance",
+    font = list(size = 14),
     annotations = list(
-      list(x = 0.17, y = 0.5,  text = "All iterations",            showarrow = FALSE, font = list(size = 12)),
-      list(x = 0.5,  y = 0.3,  text = "Baseline not significant",  showarrow = FALSE, font = list(size = 12)),
-      list(x = 0.5,  y = 0.85, text = "Baseline significant",      showarrow = FALSE, font = list(size = 12)),
-      list(x = 0.83, y = 0.15, text = "A significant deviation",   showarrow = FALSE, font = list(size = 12)),
-      list(x = 0.83, y = 0.55, text = "No significant deviations", showarrow = FALSE, font = list(size = 12))
+      list(x = 0.07, y = 0.5,  text = "All iterations",            showarrow = FALSE, xanchor = "right"),
+      list(x = 0.3, y = 0.2, text = "Baseline non-significant",          showarrow = FALSE, xanchor = "right"),
+      list(x = 0.33, y = 0.7,  text = "",                   showarrow = FALSE, xanchor = "right"),
+      list(x = 0.63, y = 0.15, text = "A significant deviation",   showarrow = FALSE, xanchor = "right"),
+      list(x = 0.66, y = 0.45, text = "",                    showarrow = FALSE, xanchor = "right"),
+      list(x = 0.99, y = 0.55, text = "",                    showarrow = FALSE, xanchor = "right"),
+      list(x = 0.99, y = 0.66, text = "",                    showarrow = FALSE, xanchor = "right"),
+      list(x = 0.99, y = 0.74, text = "",                    showarrow = FALSE, xanchor = "right"),
+      list(x = 0.99, y = 0.82, text = "",                    showarrow = FALSE, xanchor = "right"),
+      list(x = 0.99, y = 0.90, text = "",                    showarrow = FALSE, xanchor = "right"),
+      list(x = 0.99, y = 0.99, text = "",                    showarrow = FALSE, xanchor = "right"),
+      list(x = 0.99, y = 0.99, text = "",                    showarrow = FALSE, xanchor = "right")
     )
   )
+
+
+# #test
+# nodes$label <- rep("", length(nodes$label))
+# 
+# plot_ly(type = "sankey",
+#         orientation = "h",
+#         node = nodes,
+#         link = links) |>
+#   layout(
+#     annotations = list(
+#       list(x = 0.17, y = 0.5,  text = "All iterations",            showarrow = FALSE, font = list(size = 12)),
+#       list(x = 0.5,  y = 0.3,  text = "Baseline not significant",  showarrow = FALSE, font = list(size = 12)),
+#       list(x = 0.5,  y = 0.85, text = "Baseline significant",      showarrow = FALSE, font = list(size = 12)),
+#       list(x = 0.83, y = 0.15, text = "A significant deviation",   showarrow = FALSE, font = list(size = 12)),
+#       list(x = 0.83, y = 0.55, text = "No significant deviations", showarrow = FALSE, font = list(size = 12))
+#     )
+#   )
